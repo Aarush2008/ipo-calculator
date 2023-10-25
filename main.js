3c196f162c0e5c8ffa8982674f1f4a035bd0b376
Aarush2008 <98787426+Aarush2008@users.noreply.github.com>
2023-10-25 09:43:13 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// Tip Calculator
+
+// Btn Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // INPUT
+
+  let BA = +document.getElementById("BA-in").value;
+  let TP = +document.getElementById("TP-in").value;
+
+  // Process
+
+  let AmountToTip = (TP / 100) * BA;
+  let GSTAmount = BA * 0.05;
+  let Total = BA + AmountToTip + GSTAmount;
+
+  // OUTPUT
+
+  document.getElementById("output-1").innerHTML = AmountToTip.toFixed(2);
+  document.getElementById("output-2").innerHTML = GSTAmount.toFixed(2);
+  document.getElementById("output-3").innerHTML = Total.toFixed(2);
+}
